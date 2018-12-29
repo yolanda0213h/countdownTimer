5c3dccd695b0bebfadc28cbefb10de89854d11d5
yolanda0213 <yolanda0213h@gmail.com>
2018-12-29 14:19:21 +0800

    Initial Commit

---
+++ countdownTimer/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  countdownTimer
+//
+//  Created by Yolanda H. on 2018/12/29.
+//  Copyright © 2018 Yolanda H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
