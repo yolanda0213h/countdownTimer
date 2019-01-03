2e8db57daf41e84f2b133a7a1bae8d2610026ec2
yolanda0213 <yolanda0213h@gmail.com>
2019-01-03 09:59:18 +0800

    done

--- countdownTimer/FirstViewController.swift
+++ countdownTimer/FirstViewController.swift
@@ -10,11 +10,146 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var secLabel: UILabel!
+    @IBOutlet weak var minsLabel: UILabel!
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var newyearLabel: UILabel!
+    @IBOutlet weak var nowtimeLabel: UILabel!
+    var timeMove: Timer?
+    let formatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var chineseYear = "戊戌年"
+        var chineseMonth = "子月"
+        
+        formatter.dateFormat = "yyyy年dd日MM月 HH:mm:ss"
+        timeMove = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            let nowDate = Date()
+            let HH = DateFormatter()
+            HH.dateFormat = "HH"
+            let hourInt = Int(HH.string(from: nowDate))
+            let mm = DateFormatter()
+            mm.dateFormat = "mm"
+            let minsInt = Int(mm.string(from: nowDate))
+            var chineseDate = "日"
+            var chineseHour = ""
+            var chineseMins = ""
+            if minsInt! < 15 , hourInt!%2 != 0 {
+                chineseMins = "初"
+            }
+            else if minsInt! < 30 , hourInt!%2 != 0{
+                chineseMins = "初一刻"
+            }
+            else if minsInt! < 45 , hourInt!%2 != 0{
+                chineseMins = "初二刻"
+            }
+            else if minsInt! < 60 , hourInt!%2 != 0{
+                chineseMins = "初三刻"
+            }
+            else if minsInt! < 15 , hourInt!%2 == 0 {
+                chineseMins = "正"
+            }
+            else if minsInt! < 30 , hourInt!%2 == 0 {
+                chineseMins = "正一刻"
+            }
+            else if minsInt! < 45 , hourInt!%2 == 0 {
+                chineseMins = "正二刻"
+            }
+            else if minsInt! < 60 , hourInt!%2 == 0 {
+                chineseMins = "正三刻"
+            }
+            if hourInt! < 01 {
+                chineseHour = "早子"
+            }
+            else if hourInt! < 03 {
+                chineseHour = "丑"
+            }
+            else if hourInt! < 05 {
+                chineseHour = "寅"
+            }
+            else if hourInt! < 07 {
+                chineseHour = "卯"
+            }
+            else if hourInt! < 09 {
+                chineseHour = "辰"
+            }
+            else if hourInt! < 11 {
+                chineseHour = "巳"
+            }
+            else if hourInt! < 13 {
+                chineseHour = "午"
+            }
+            else if hourInt! < 15 {
+                chineseHour = "未"
+            }
+            else if hourInt! < 17 {
+                chineseHour = "申"
+            }
+            else if hourInt! < 19 {
+                chineseHour = "酉"
+            }
+            else if hourInt! < 21 {
+                chineseHour = "戌"
+            }
+            else if hourInt! < 23 {
+                chineseHour = "亥"
+            }
+            else if hourInt! >= 23 {
+                chineseHour = "晚子"
+            }
+            
+            let yearDateComponents = DateComponents(calendar: Calendar.current, year: 2019, month: 02, day: 05, hour: 00, minute: 00, second: 00)
+            let yearDate = yearDateComponents.date
+            let interval = yearDate!.timeIntervalSince(nowDate) + 1
+            
+            let sky10 = ["甲","乙","丙","丁","戊","己","庚","辛","壬","癸"]
+            let groud12 = ["子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥"]
+            var i = 0
+            if Int(interval/86400) > 60 {
+                i = Int(interval/86400)%60
+                if i < 9 {
+                    chineseDate = sky10[9-i] + groud12[9-i] + chineseDate
+                }
+                else {
+                    let a = 9-(Int(interval/86400)%60 - 9)%10
+                    let b = 11-(Int(interval/86400)%60 - 9)%12
+                    chineseDate = sky10[a] + groud12[b] + chineseDate
+                }
+            }
+            else{
+             if Int(interval/86400) < 9 {
+                chineseDate = sky10[9-i] + groud12[9-i] + chineseDate
+             }
+             else if Int(interval/86400) >= 9 {
+                let a = 9-(Int(interval/86400) - 9)%10
+                let b = 11-(Int(interval/86400) - 9)%12
+                chineseDate = sky10[a] + groud12[b] + chineseDate
+             }
+             
+            }
+            if (Int(interval/86400) - 9) == 2 {
+                chineseMonth = "丑月"
+            }
+            if (Int(interval/86400) - 9) == 1 {
+                chineseMonth = "寅月"
+            }
+            let chineseTime = chineseYear + "-" + chineseDate + "-" + chineseMonth + " \n" + chineseHour + "時" + chineseMins
+            self.nowtimeLabel.text = self.formatter.string(from: nowDate) + "\n\n" + chineseTime
+            
+            self.newyearLabel.text = self.formatter.string(from: yearDate!) + "\n\n己亥年-癸酉日-寅月 早子時正"
+            
+            self.dayLabel.text = String(Int(interval/86400))
+            self.hourLabel.text = String(Int(interval/3600)%24)
+            self.minsLabel.text = String(Int(interval/60)%60)
+            self.secLabel.text = String(Int(interval)%60)
+        })
     }
-
+    override func viewDidDisappear(_ animated: Bool) {
+        //timeMove?.invalidate()
+    }
 
 }
 
--- countdownTimer/SecondViewController.swift
+++ countdownTimer/SecondViewController.swift
@@ -8,13 +8,82 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+class SecondViewController: UIViewController {
+    var timerSche: Timer?
+    var timerMove: Timer?
+    let formatter = DateFormatter()
+    @IBOutlet weak var closeView: UIView!
+    @IBOutlet weak var sec: UILabel!
+    @IBOutlet weak var mins: UILabel!
+    @IBOutlet weak var hour: UILabel!
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var doomsButton: UIButton!
+    @IBOutlet weak var nowDate: UILabel!
+    @IBOutlet weak var datePicher: UIDatePicker!
+    @IBOutlet weak var sendButton: UIButton!
+    func timeSche(repeats: Bool, runtime: TimeInterval){
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let doomsTime = datePicher.date
+        let doomsLabel = formatter.string(from: doomsTime)
+        doomsButton.setTitle(doomsLabel, for: UIControl.State.normal)
+        timerSche = Timer.scheduledTimer(withTimeInterval: runtime, repeats: repeats, block: { (_) in
+            let nowTime = Date()
+            let doomsTime = self.datePicher.date
+            let interval = doomsTime.timeIntervalSince(nowTime)
+            let secInt = Int(interval)%60
+            let dayInt = Int(interval/86400)
+            self.sec.text = ("\(secInt)")
+            self.mins.text = String(Int(interval/60)%60)
+            self.hour.text = String(Int(interval/3600)%24)
+            self.day.text = ("\(dayInt)")
+        })
+    }
+    func timemove(){
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timerMove = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            let nowTime = Date()
+            let time = self.formatter.string(from: nowTime)
+            self.nowDate.text = time
+            
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        timemove()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func funcDooms(_ sender: UIButton) {
+        sendButton.isHidden = false
+        datePicher.isHidden = false
+        closeView.isHidden = false
+    }
+    @IBAction func funcButton(_ sender: UIButton) {
+        sendButton.isHidden = true
+        datePicher.isHidden = true
+        closeView.isHidden = true
+        timemove()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let doomsTime = datePicher.date
+        let doomsLabel = formatter.string(from: doomsTime)
+        doomsButton.setTitle(doomsLabel, for: UIControl.State.normal)
+    }
+    @IBAction func funcDatePicher(_ sender: UIDatePicker) {
+      sender.minimumDate = Date()
+    }
+    
+    @IBAction func stopButton(_ sender: UIButton) {
+        if sender.currentTitle == "開始倒數" {
+            timeSche(repeats: true, runtime: 1)
+            sender.setTitle("暫停倒數", for: UIControl.State.normal)
+        }
+        else if sender.currentTitle == "暫停倒數" {
+            timerSche?.invalidate()
+            sender.setTitle("開始倒數", for: UIControl.State.normal)
+        }
+        
+    }
 }
 
